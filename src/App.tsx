ff2a8e30bc194f228b5e0587862c45c1216c82eb
nicolasmoovin <nicolas.jardin@moovin.com.br>
2022-08-26 17:12:22 -0300

    Para instalar material UI
    Para utilizar CSSBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { CssBaseline } from '@mui/material'
 import { BrowserRouter } from 'react-router-dom'
 import Router from './routes/Router'
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <BrowserRouter>
         <Router />
       </BrowserRouter>
