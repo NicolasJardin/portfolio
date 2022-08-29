7273a304c87c6f7ace6d19a2026d1a5a8195d16d
nicolasmoovin <nicolas.jardin@moovin.com.br>
2022-08-29 12:14:56 -0300

    Para ajustar cor dos temas

--- src/modules/theme/themes/LightTheme/LightTheme.ts
+++ src/modules/theme/themes/LightTheme/LightTheme.ts
@@ -3,17 +3,14 @@ import { createTheme } from '@mui/material'
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#fff',
-      light: '#fff',
-      dark: '#fff'
+      main: '#42a5f5',
+      light: '#80d6ff',
+      dark: '#0077c2'
     },
     secondary: {
-      main: '#ea80fc',
-      light: '#ffb2ff',
-      dark: '#b64fc8'
-    },
-    text: {
-      primary: '#fff'
+      main: '#bdbdbd',
+      light: '#efefef',
+      dark: '#8d8d8d'
     }
   }
 })
--- src/modules/theme/themes/MainTheme/MainTheme.ts
+++ src/modules/theme/themes/MainTheme/MainTheme.ts
@@ -3,14 +3,14 @@ import { createTheme } from '@mui/material'
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#424242',
-      light: '#6d6d6d',
+      main: '#352728',
+      light: '#323232',
       dark: '#1b1b1b'
     },
     secondary: {
-      main: '#ea80fc',
-      light: '#ffb2ff',
-      dark: '#b64fc8'
+      main: '#8E5D52',
+      light: '#C4A484	',
+      dark: '#6F4E37'
     },
     text: {
       primary: '#fff'
