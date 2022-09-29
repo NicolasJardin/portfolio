82b26453e2ba804386f41dc36a04268015972130
nicolasmoovin <nicolas.jardin@moovin.com.br>
2022-09-29 15:51:51 -0300

    Para retirar arquivos styles e efeito do header

--- src/modules/layout/components/Header/Header.tsx
+++ src/modules/layout/components/Header/Header.tsx
@@ -1,9 +1,18 @@
-import { Typography } from '@mui/material'
+import { Typography, styled } from '@mui/material'
 import WaterText from 'modules/animation/components/WaterText'
 import Link from 'modules/link/Link'
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { HeaderContainer } from './Styles'
+
+export const HeaderContainer = styled('header', { name: 'HeaderContainer' })(({ theme }) => ({
+  width: '100%',
+  height: 60,
+  display: 'flex',
+  justifyContent: 'space-evenly',
+  alignItems: 'center',
+  background: theme.palette.background.default,
+  borderBottom: `1px solid ${theme.palette.divider}`
+}))
 
 const Header: FC = () => {
   const { t } = useTranslation()
--- src/modules/link/Link/Link.tsx
+++ src/modules/link/Link/Link.tsx
@@ -1,8 +1,18 @@
-import { LinkBaseProps } from '@mui/material'
+import { LinkBaseProps, Link as LinkMui, styled } from '@mui/material'
 import { FC } from 'react'
-import { LinkStyled } from './Styles'
 
 type LinkProps = LinkBaseProps
+
+export const LinkStyled = styled(LinkMui)(({ theme }) => ({
+  color: theme.palette.text.primary,
+  transition: theme.transitions.create('color'),
+  borderBottom: '2px solid transparent',
+
+  '&:hover': {
+    color: theme.palette.primary.main,
+    borderColor: theme.palette.primary.main
+  }
+}))
 
 const Link: FC<LinkProps> = ({ children, ...props }) => {
   return (
