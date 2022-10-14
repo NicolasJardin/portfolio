2a824d311a0fdb3ad9de2528290935f9be413279
nicolasmoovin <nicolas.jardin@moovin.com.br>
2022-10-14 14:24:34 -0300

    Para alinhar header e adicionar formações

--- src/modules/layout/components/Header/Header.tsx
+++ src/modules/layout/components/Header/Header.tsx
@@ -7,17 +7,17 @@ import { Link } from 'react-router-dom'
 export const HeaderContainer = styled('header', { name: 'HeaderContainer' })(({ theme }) => ({
   width: '100%',
   height: 60,
-  display: 'flex',
-  justifyContent: 'space-evenly',
   alignItems: 'center',
   background: theme.palette.background.default,
   borderBottom: `1px solid ${theme.palette.divider}`,
-  padding: 20
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr'
 }))
 
 const LinkStyled = styled(Link, {
   name: 'LinkStyled'
 })(({ theme }) => ({
+  justifySelf: 'center',
   color: theme.palette.primary.light,
   textDecoration: 'none',
 
@@ -27,19 +27,17 @@ const LinkStyled = styled(Link, {
   }
 }))
 
-//TODO AJUSTAR LINKS
 const Header: FC = () => {
   const { t } = useTranslation()
 
   return (
     <HeaderContainer>
-      <LinkStyled to="/">{t('Inicio')}</LinkStyled>
-      <LinkStyled to="/">{t('Sobre mim')}</LinkStyled>
-
+      <LinkStyled to="/">{t('Formações')}</LinkStyled>
+      <LinkStyled to="/project">{t('Projetos')}</LinkStyled>
       <LinkStyled to="/">
         <WaterText text={<Typography variant="h4">NJ</Typography>} />
       </LinkStyled>
-      <LinkStyled to="/project">{t('Projetos')}</LinkStyled>
+      <LinkStyled to="/">{t('Sobre mim')}</LinkStyled>
       <LinkStyled to="/">{t('Habilidades')}</LinkStyled>
     </HeaderContainer>
   )
