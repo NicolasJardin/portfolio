47145d3d39424787e7df349123b93f220c5b0311
nicolas-1672953028 <nicolasjdev@gmail.com>
2023-01-30 19:15:21 -0300

    feat: Adicionar projeto letmeask

--- src/modules/project/components/ProjectCard/ProjectCard.tsx
+++ src/modules/project/components/ProjectCard/ProjectCard.tsx
@@ -1,13 +1,28 @@
-import { Card, CardContent, styled } from '@mui/material'
+import { Card, CardContent, Stack, styled, Typography } from '@mui/material'
 import ColorText from 'modules/animation/components/ColorText/ColorText'
 import { useTranslation } from 'react-i18next'
+
+type ProjectCardProps = {
+  project?: {
+    title: string
+    description: string
+    image: string
+    url?: string
+  }
+}
 
 const ProjectCardRoot = styled(Card, {
   name: 'ProjectCard',
   slot: 'Root'
-})({
-  height: 300
-})
+})<{ hasUrl: boolean }>(({ theme, hasUrl }) => ({
+  transition: theme.transitions.create('transform'),
+  height: 300,
+  cursor: hasUrl ? 'pointer' : '',
+
+  '&:hover': {
+    transform: 'scale(1.02)'
+  }
+}))
 
 const CardContentWithoutProject = styled(CardContent, {
   name: 'CardContentWithoutProject'
@@ -31,16 +46,55 @@ const SoonContent = styled('div', {
   background: theme.palette.primary.main
 }))
 
-export default function ProjectCard() {
+const Image = styled('img', { name: 'Image' })({
+  width: '100%',
+  height: '60%',
+  objectFit: 'cover'
+})
+
+const Title = styled('div', {
+  name: 'Title'
+})(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  width: '100%'
+}))
+
+const ProjectContent = styled(Stack, {
+  name: 'ProjectContent'
+})({
+  height: '100%',
+  textAlign: 'center'
+})
+
+export default function ProjectCard({ project }: ProjectCardProps) {
   const { t } = useTranslation()
 
   return (
-    <ProjectCardRoot>
-      <CardContentWithoutProject>
-        <SoonContent>
-          <ColorText>{t('Em breve')}</ColorText>
-        </SoonContent>
-      </CardContentWithoutProject>
+    <ProjectCardRoot
+      hasUrl={!!project?.url}
+      onClick={() => !!project?.url && window.open(project.url)}
+    >
+      {project ? (
+        <ProjectContent>
+          <Image src={project.image} alt="project" />
+
+          <Stack gap={1}>
+            <Title>
+              <Typography variant="h6">{project.title}</Typography>
+            </Title>
+
+            <Typography variant="subtitle1" sx={{ padding: 0.5 }}>
+              {project.description}
+            </Typography>
+          </Stack>
+        </ProjectContent>
+      ) : (
+        <CardContentWithoutProject>
+          <SoonContent>
+            <ColorText>{t('Em breve')}</ColorText>
+          </SoonContent>
+        </CardContentWithoutProject>
+      )}
     </ProjectCardRoot>
   )
 }
--- src/modules/project/components/ProjectList/ProjectList.tsx
+++ src/modules/project/components/ProjectList/ProjectList.tsx
@@ -1,14 +1,26 @@
 import { Grid } from '@mui/material'
+import { useTranslation } from 'react-i18next'
 import ProjectCard from '../ProjectCard'
+import letmeaskImage from 'assets/img/letmeask.png'
 
 type ProjectListProps = {}
 
 export default function ProjectList(props: ProjectListProps) {
+  const { t } = useTranslation()
+
   return (
     <Grid container spacing={6}>
       <Grid item xs={4}>
-        <ProjectCard />
+        <ProjectCard
+          project={{
+            title: 'Letmeask',
+            description: t('Projeto desenvolvido durante a Next Level Week Together'),
+            image: letmeaskImage,
+            url: 'https://letmeask-68024.web.app/'
+          }}
+        />
       </Grid>
+
       <Grid item xs={4}>
         <ProjectCard />
       </Grid>
